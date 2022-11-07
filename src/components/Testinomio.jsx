c8a2e0470c4e0ee3bb5f2f2f55b591a70e4cb109
Joel8d <joel8d@gmail.com>
2022-11-07 10:14:44 -0500

    First commit. Profile image and more

---
+++ src/components/Testinomio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Testimonio(){
+    return(
+        <div className="contenedor-testimonio">
+            <img className="image-testimonio" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile"/>
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">Joel Canul en Dallas</p>
+                <p className="cargo-testimonio">FrontEnd Developer en Google</p>
+                <p className="texto-testimonio">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, consequatur molestias. Iusto corrupti soluta voluptate repudiandae delectus quo architecto, aliquam nam tempore et maxime suscipit ea quam quis labore? Aspernatur?</p>       
+            </div>
+        </div>
+    )
+}
